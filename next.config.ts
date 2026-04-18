7e833983de1ac0a193c7059e7f9d78f407a33020
DouliaGroup <douliagroup@gmail.com>
2026-04-18 07:28:27 +0100

    GODLOVE

--- next.config.ts
+++ next.config.ts
@@ -20,3 +20,12 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Cela permet à Vercel de compiler même s'il y a des avertissements ESLint
+    ignoreDuringBuilds: true,
+  },
+}
+module.exports = nextConfig
